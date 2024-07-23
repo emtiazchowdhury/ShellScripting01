43780bf2ad4665b318fc05af45f43302062c930a
EC2 Default User <ec2-user@ip-172-31-84-220.ec2.internal>
2024-07-23 04:35:16 +0000

    Update

--- parsingVariablefromCommandLine.sh
+++ parsingVariablefromCommandLine.sh
@@ -6,6 +6,5 @@ echo "Script name: $1"
 echo "Script name: $2"
 #echo "All arguments: $@"
 echo "All arguments: $*"
-echo "Script ran"
 
 #Run this code from command line and enter the number of arguments
