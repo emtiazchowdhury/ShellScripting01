6caadf485059f439ee2d6a7163bb73e20e1f17e2
emtiazchowdhury <emtiazchow@gmail.com>
2024-07-26 03:45:23 +0000

    update

--- ShellScripts/ShellScriptingGuide/misc/test.sh
+++ ShellScripts/ShellScriptingGuide/misc/test.sh
@@ -5,3 +5,12 @@ nameofSeeds=("pepper" "Squash" "Leafy green" "Fruits")
 for seed in "${nameofSeeds[@]}"; do
     echo "I want to grow $seed"
 done
+
+REGION=$1
+
+if [ -z "$REGION" ]; then
+    echo "Please specify a region."
+    exit 1
+fi
+
+aws ec2 describe-instances --region $REGION
